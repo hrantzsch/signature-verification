05c536eaf2eb47e26db1bad55739711c54a7a60a
Hannes Rantzsch <hannes.rantzsch@student.hpi.de>
2015-11-10 12:03:20 +0100

    crop and resize

--- tools/prepimage.py
+++ tools/prepimage.py
@@ -10,6 +10,7 @@ def _find_min(image, row=0):
     else:
         return _find_min(image, row+1)
 
+
 def _find_max(image, row):
     if np.any(image[row]):
         return row
@@ -23,66 +24,47 @@ def min_max(image):
     img_rot = np.rot90(image)
     x_min = _find_min(img_rot)
     x_max = _find_max(img_rot, img_rot.shape[0]-1)
-    print((x_min, x_max), (y_min,y_max))
-    return (x_min, x_max), (y_min,y_max)
+    return (x_min, x_max), (y_min, y_max)
 
 
 def crop(image, ratio):
-    dest_w, dest_h = ratio[0], ratio[1]
-
     thresh = threshold_otsu(image)
-    binary = np.asarray(image <= 255)
+    binary = image <= thresh
+
     (x_min, x_max), (y_min, y_max) = min_max(binary)
-    # src_w, src_h = x_max-x_min, y_max-y_min
+    src_w, src_h = x_max-x_min, y_max-y_min
 
-    # if src_w / src_h == dest_w / dest_h:
-    return fromimage(image)[x_min:x_max, y_min:y_max]
-    #
-    # if (src_w / src_h) > (dest_w / dest_h):  # need to pad height
-    #     print("A")
-    #     pad_y = (dest_h / dest_w) * src_w - src_h
-    #     return image[(y_min-pad_y/2):(y_max+pad_y/2), x_min:x_max]
-    # else:
-    #     print("B")
-    #     pad_x = (dest_w / dest_h) * src_h - src_w
-    #     return image[(x_min-pad_x/2):(x_max-pad_x/2), y_min:y_max]
+    if (src_w / src_h) > ratio:  # pad height
+        pad = (1/ratio) * src_w - src_h
+        y_min -= pad / 2
+        y_max += pad / 2
+    elif (src_w / src_h) < ratio:
+        pad = ratio * src_h - src_w
+        x_min -= pad / 2
+        x_max += pad / 2
 
-
-#
-# def prep_image(image):
-#     image = crop(image)
-#     image = imresize(image, )
+    image = np.pad(image, 500, mode='edge')  # yes, it's ugly
+    return image[y_min+500:y_max+500, x_min+500:x_max+500]
 
 
 if __name__ == "__main__":
-    from skimage import io
     from mkdata_random import get_files
+    import os
 
     images = list(get_files("/home/hannes/Data/firmasSINTESISmanuscritas"))
     num_imgs = len(images)
 
     image = images[0]
-    ratio = 200/120
 
-
-    avg_x = 0
-    avg_y = 0
     count = 0
     for f in images:
         with Image.open(f) as image:
-            print("{}/{} - {}".format(count, num_imgs, f))#, end='\r')
+            print("{}/{} - {}".format(count, num_imgs, f), end='\r')
             count += 1
-            image = crop(image, (1.025,2))
-            imsave("/home/hannes/tmp/images/{}.png".format(count), image)
+            image = crop(fromimage(image), (2.0 / 1))
+            image = imresize(image, (100, 200), mode='L')
+            imsave("/home/hannes/tmp/images/{}".format(os.path.basename(f)), image)
 
 
-    #     thresh = threshold_otsu(image)
-    #     binary = np.asarray(image <= thresh)
-    #     (x_min, x_max), (y_min,y_max) = min_max(binary)
-    #     avg_x += (x_max - x_min)
-    #     avg_y += (y_max - y_min)
-    #     count += 1
-    #
-    # print()
     # print (avg_x, avg_y, avg_x / num_imgs, avg_y / num_imgs)
     # 86551127 168976381 400.69966203703706 782.2980601851851
