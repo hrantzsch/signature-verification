964fb47e99ae0d9be65d9f5d55e6edd68d2f3c47
Hannes Rantzsch <hannes.rantzsch@student.hpi.de>
2015-11-20 16:58:27 +0100

    l2 normalization

---
+++ distance/l2normalization.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+from chainer import function
+from chainer.utils import type_check
+
+
+class L2Normalization(function.Function):
+
+    """"""
+
+    def __init__(self, scale=1):
+        self.scale = scale
+
+    def check_type_forward(self, in_types):
+        type_check.expect(in_types.size() == 1)
+        x_type, = in_types
+
+        type_check.expect(
+            x_type.dtype == np.float32,
+            x_type.ndim >= 2,
+        )
+
+    def forward_cpu(self, inputs):
+        x, = inputs
+        N = (x.shape[0])
+        self.norm = np.zeros(N, dtype=np.int32)
+        # TODO there's a better way
+        for i in range(N):
+            self.norm[i] = x[i].dot(x[i]) * self.scale
+        return self.norm,
+
+    def backward(self, inputs, gy):
+        # gradient is simply 2x * self.scale
+        err = 2 * inputs * self.scale
+        return err * gy[0],
+
+
+def l2_normalization(x0, scale=1):
+    """"""
+    return L2Normalization(scale)(x0)
