0dd2745b9abb7d0132b799c9f5949daf57a7cd3d
Hannes Rantzsch <hannes.rantzsch@student.hpi.de>
2015-11-30 14:21:49 +0100

    chainer serializers in verification

--- distance/verify.py
+++ distance/verify.py
@@ -5,6 +5,9 @@ from scipy.misc import imread
 
 import chainer
 from chainer import cuda
+from chainer import serializers
+
+from embednet import EmbedNet
 
 parser = argparse.ArgumentParser()
 parser.add_argument('model',
@@ -13,12 +16,20 @@ parser.add_argument('sample_a',
                     help='Path to first signature')
 parser.add_argument('sample_b',
                     help='Path to second signature')
+parser.add_argument('--gpu', '-g', default=-1, type=int,
+                    help='GPU ID (negative value indicates CPU)')
+
 args = parser.parse_args()
 
-model = pickle.load(open(args.model, 'rb'))
-model.to_gpu(0)
+model = EmbedNet()
 
-xp = cuda.cupy
+if args.gpu >= 0:
+    cuda.check_cuda_available()
+    model.to_gpu(args.gpu)
+xp = cuda.cupy if args.gpu >= 0 else np
+
+print('Load model from', args.model)
+serializers.load_hdf5(args.model, model)
 
 samples = [xp.asarray(imread(args.sample_a).astype(xp.float32)[xp.newaxis, xp.newaxis, ...]),
            xp.asarray(imread(args.sample_b).astype(xp.float32)[xp.newaxis, xp.newaxis, ...])]
