decfe815a74acae75699d4beecb0beab3a27e301
Hannes Rantzsch <hannes.rantzsch@student.hpi.de>
2015-12-19 10:08:05 +0100

    fix dataloader for triplet training

--- distance/data_loader.py
+++ distance/data_loader.py
@@ -9,12 +9,12 @@ class DataLoader(object):
     analogously to the gpds synthetic dataset.
     """
 
-    def __init__(self, data_dir, array_module, image_ext='.jpg'):
+    def __init__(self, data_dir, array_module, image_ext='.png'):
         self.data_dir = data_dir
         self.xp = array_module
         self.image_ext = image_ext
 
-    def get_signature_path(self, person, sign_num, sample):
+    def get_signature_path(self, person, sign_num, variation):
         """Assemble a filename for a signature like 'cf-001-18-05.png' and
            return the full path to the signature image."""
         directory = os.path.join(self.data_dir, "{:03d}".format(person))
@@ -24,11 +24,11 @@ class DataLoader(object):
         else:
             prefix = "c"
         fname = "{}-{:03d}-{:02d}-{:02d}{}".format(
-            prefix, person, sign_num, sample, self.image_ext)
+            prefix, person, sign_num, variation, self.image_ext)
         return os.path.join(directory, fname)
 
-    def load_image(self, person, sign_num, sample):
-        path = self.get_signature_path(person, sign_num, sample)
+    def load_image(self, person, sign_num, variation):
+        path = self.get_signature_path(person, sign_num, variation)
         return imread(path).astype(self.xp.float32)[self.xp.newaxis, ...]
 
 
@@ -44,10 +44,13 @@ class TripletLoader(DataLoader):
     def get_batch(self, anchor_id, num_triplets):
         """Make a batch using person <anchor_id> as anchor."""
         anchor_samples = list(range(1, 25)) if self.skilled_forgeries else list(range(1, 55))
+
         np.random.shuffle(anchor_samples)
 
-        # pop anchor_sample, REMOVING it from the remaining anchor_samples
-        anchor_sample = anchor_samples.pop()
+        # pop anchor_sample, removing it from the remaining anchor_samples
+        # the variation of the anchor also needs to be fix
+        anchor_sign_num = anchor_samples.pop()
+        anchor_variation = np.random.randint(1, 21)
 
         neg_ids = list(range(1, self.num_classes+1))
         neg_ids.remove(anchor_id)
@@ -56,17 +59,19 @@ class TripletLoader(DataLoader):
                         np.random.choice(list(range(1, 55))))
                        for i in range(num_triplets)]
 
+        # for both positive and negative samples we always choose a random variation
         # repeat anchor sample
-        a = self.xp.array([self.load_image(anchor_id, anchor_sample)] * num_triplets,
-                          dtype=self.xp.float32)
+        a = self.xp.array(
+            [self.load_image(anchor_id, anchor_sign_num, anchor_variation)] *
+            num_triplets, dtype=self.xp.float32)
         # generate <num_triplets> p's randomly sampled from remaining anchor_samples
-        p = self.xp.array([self.load_image(anchor_id, np.random.choice(anchor_samples))
-                           for _ in range(num_triplets)],
-                          dtype=self.xp.float32)
+        p = self.xp.array(
+            [self.load_image(anchor_id, np.random.choice(anchor_samples), np.random.randint(1, 21))
+             for _ in range(num_triplets)], dtype=self.xp.float32)
         # negative samples from remaining neg_ids
-        n = self.xp.array([self.load_image(np.random.choice(neg_ids), np.random.choice(list(range(1, 55))))
-                           for _ in range(num_triplets)],
-                          dtype=self.xp.float32)
+        n = self.xp.array(
+            [self.load_image(np.random.choice(neg_ids), np.random.choice(list(range(1, 55))), np.random.randint(1, 21))
+             for _ in range(num_triplets)], dtype=self.xp.float32)
         return self.xp.concatenate([a, p, n])
 
 
@@ -74,7 +79,6 @@ class LabelDataLoader(DataLoader):
 
     def get_batch(self, tuples):
         """Return two batches data, labels."""
-        # import pdb; pdb.set_trace()
 
         data = self.xp.array([self.load_image(user, sign_num, sample)
                               for (user, sign_num, sample) in tuples],
