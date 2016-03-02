01b5ddba8da0a9c0d39abf9a1d00786fb62a4ed0
Hannes Rantzsch <hannes.rantzsch@student.hpi.de>
2016-03-02 10:44:20 +0100

    plot distance

--- tools/plot.py
+++ tools/plot.py
@@ -6,10 +6,13 @@ from itertools import chain
 def parse(logfile):
     loss_train = []
     loss_test = []
+    section_loss = []
     acc_train = []
     acc_test = []
-    section_loss = []
     section_acc = []
+    dist_train = []
+    dist_test = []
+    section_dist = []
     with open(args.logfile, 'r') as logfile:
         for line in logfile:
             if line.startswith('#'):  # skip comments, e.g. the header
@@ -21,6 +24,9 @@ def parse(logfile):
                 if len(section_acc) > 0:
                     acc_test.append(section_acc)
                     section_acc = []
+                if len(section_dist) > 0:
+                    dist_test.append(section_dist)
+                    section_dist = []
             elif 'test' in line:
                 if len(section_loss) > 0:
                     loss_train.append(section_loss)
@@ -28,16 +34,24 @@ def parse(logfile):
                 if len(section_acc) > 0:
                     acc_train.append(section_acc)
                     section_acc = []
+                if len(section_dist) > 0:
+                    dist_train.append(section_dist)
+                    section_dist = []
             else:
-                section_loss.append(float(line.split(',')[1]))
-                if 'None' not in line:
-                    section_acc.append(float(line.split(',')[2]))
+                it, loss, acc, dist = line.split(',')
+                section_loss.append(float(loss))
+                if acc is not None:
+                    section_acc.append(float(acc))
+                if dist is not None:
+                    section_dist.append(float(dist))
     if len(section_loss) > 0:
         loss_test.append(section_loss)
     if len(section_acc) > 0:
         acc_test.append(section_acc)
+    if len(section_dist) > 0:
+        dist_test.append(section_dist)
 
-    return loss_train, loss_test, acc_train, acc_test
+    return loss_train, loss_test, acc_train, acc_test, dist_train, dist_test
 
 
 def avg(l):
@@ -45,9 +59,9 @@ def avg(l):
 
 
 def plot_avg(logfile):
-    loss_train, loss_test, acc_train, acc_test = parse(logfile)
+    loss_train, loss_test, acc_train, acc_test, dist_train, dist_test = parse(logfile)
 
-    f, axarr = plt.subplots(2, sharex=True)
+    f, axarr = plt.subplots(3, sharex=True)
     x = list(range(1, len(loss_train)+1))
     # x = list(range(len(loss_train)))  # old style starting at epoch 0
 
@@ -56,10 +70,17 @@ def plot_avg(logfile):
     # axarr[0].set_ylim([0.45, 0.55])
     axarr[0].set_title("loss")
     axarr[0].legend(loc='upper right')
+
+    axarr[1].plot(x, list(map(avg, acc_train)), '.-')
     if len(acc_train) > 0:
-        axarr[1].plot(x, list(map(avg, acc_train)), '.-')
-    axarr[1].plot(x, list(map(avg, acc_test)), 'g.-')
+        axarr[1].plot(x, list(map(avg, acc_test)), 'g.-')
     axarr[1].set_title("acc")
+
+    if len(dist_train) > 0:
+        axarr[2].plot(x, list(map(avg, dist_train)), '.-')
+        if len(dist_test) > 0:
+            axarr[2].plot(x, list(map(avg, dist_test)), 'g.-')
+        axarr[2].set_title("dist")
     # axarr[1].set_ylim([0.2, 0.3])
 
     plt.show()
