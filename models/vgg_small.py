541158f006b6ca16e441f95663409e788f28ad67
Hannes Rantzsch <hannes.rantzsch@student.hpi.de>
2016-05-28 13:48:49 +0200

    vgg_small fewer convs

--- models/vgg_small.py
+++ models/vgg_small.py
@@ -4,13 +4,12 @@ import chainer.links as L
 
 
 class VGGSmall(chainer.Chain):
-
     def __init__(self):
         super(VGGSmall, self).__init__(
             conv1_1=L.Convolution2D(1, 96, 11, stride=3, pad=1),
-            conv1_2=L.Convolution2D(96, 128, 3, stride=1, pad=1),
+            conv1_2=L.Convolution2D(96, 96, 3, stride=1, pad=1),
 
-            conv2_1=L.Convolution2D(64, 128, 5, stride=1, pad=1),
+            conv2_1=L.Convolution2D(96, 128, 5, stride=1, pad=1),
             conv2_2=L.Convolution2D(128, 128, 3, stride=1, pad=1),
 
             conv3_1=L.Convolution2D(128, 256, 3, stride=1, pad=1),
@@ -20,38 +19,38 @@ class VGGSmall(chainer.Chain):
             conv4_1=L.Convolution2D(256, 512, 3, stride=1, pad=1),
             conv4_2=L.Convolution2D(512, 512, 3, stride=1, pad=1),
             conv4_4=L.Convolution2D(512, 512, 3, stride=1, pad=1),
+            #
+            # conv5_1=L.Convolution2D(512, 512, 3, stride=1, pad=1),
+            # conv5_2=L.Convolution2D(512, 512, 3, stride=1, pad=1),
+            # conv5_3=L.Convolution2D(512, 512, 3, stride=1, pad=1),
 
-            conv5_1=L.Convolution2D(512, 512, 3, stride=1, pad=1),
-            conv5_2=L.Convolution2D(512, 512, 3, stride=1, pad=1),
-            conv5_3=L.Convolution2D(512, 512, 3, stride=1, pad=1),
-
-            fc1=L.Linear(1024, 256),
-            fc2=L.Linear(256, 64)
+            fc1=L.Linear(4096, 1024),
+            fc2=L.Linear(1024, 128)
         )
 
     def __call__(self, x):
         h = self.conv1_1(x)
         h = self.conv1_2(h)
-        h = F.max_pooling_2d(h, 2, stride=2, pad=1),
+        h = F.max_pooling_2d(h, 2, stride=2, pad=1)
 
         h = self.conv2_1(h)
         h = self.conv2_2(h)
-        h = F.max_pooling_2d(h, 2, stride=2, pad=1),
+        h = F.max_pooling_2d(h, 2, stride=2, pad=1)
 
         h = self.conv3_1(h)
         h = self.conv3_2(h)
         h = self.conv3_3(h)
-        h = F.max_pooling_2d(h, 2, stride=2, pad=0),
+        h = F.max_pooling_2d(h, 2, stride=2, pad=0)
 
         h = self.conv4_1(h)
         h = self.conv4_2(h)
         h = self.conv4_4(h)
-        h = F.max_pooling_2d(h, 2, stride=2, pad=0),
-
-        h = self.conv5_1(h)
-        h = self.conv5_2(h)
-        h = self.conv5_3(h)
-        h = F.max_pooling_2d(h, 2, stride=2, pad=0),
+        h = F.max_pooling_2d(h, 2, stride=2, pad=0)
+        #
+        # h = self.conv5_1(h)
+        # h = self.conv5_2(h)
+        # h = self.conv5_3(h)
+        # h = F.max_pooling_2d(h, 2, stride=2, pad=0)
 
         h = self.fc1(h)
         h = self.fc2(h)
