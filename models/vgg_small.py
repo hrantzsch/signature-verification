1b289b67d7c4b2827507c9a19a98a9ef10f0d60e
Hannes Rantzsch <hannes.rantzsch@student.hpi.de>
2016-05-30 17:25:55 +0200

    tidy up vgg_small model

--- models/vgg_small.py
+++ models/vgg_small.py
@@ -3,9 +3,11 @@ import chainer.functions as F
 import chainer.links as L
 
 
-class VGGSmall(chainer.Chain):
+class VGGSmallConv(chainer.Chain):
+    """The convolutional part of VGGSmall, to be plugged under FC layers for
+       either classifcation or tripletloss."""
     def __init__(self):
-        super(VGGSmall, self).__init__(
+        super(VGGSmallConv, self).__init__(
             conv1_1=L.Convolution2D(1, 96, 11, stride=3, pad=1),
             conv1_2=L.Convolution2D(96, 96, 3, stride=1, pad=1),
 
@@ -18,14 +20,7 @@ class VGGSmall(chainer.Chain):
 
             conv4_1=L.Convolution2D(256, 512, 3, stride=1, pad=1),
             conv4_2=L.Convolution2D(512, 512, 3, stride=1, pad=1),
-            conv4_4=L.Convolution2D(512, 512, 3, stride=1, pad=1),
-            #
-            # conv5_1=L.Convolution2D(512, 512, 3, stride=1, pad=1),
-            # conv5_2=L.Convolution2D(512, 512, 3, stride=1, pad=1),
-            # conv5_3=L.Convolution2D(512, 512, 3, stride=1, pad=1),
-
-            fc1=L.Linear(4096, 1024),
-            fc2=L.Linear(1024, 128)
+            conv4_4=L.Convolution2D(512, 512, 3, stride=1, pad=1)
         )
 
     def __call__(self, x):
@@ -46,13 +41,20 @@ class VGGSmall(chainer.Chain):
         h = self.conv4_2(h)
         h = self.conv4_4(h)
         h = F.max_pooling_2d(h, 2, stride=2, pad=0)
-        #
-        # h = self.conv5_1(h)
-        # h = self.conv5_2(h)
-        # h = self.conv5_3(h)
-        # h = F.max_pooling_2d(h, 2, stride=2, pad=0)
 
+        return h
+
+
+class VGGSmall(chainer.Chain):
+    def __init__(self):
+        super(VGGSmall, self).__init__(
+            conv=VGGSmallConv(),
+            fc1=L.Linear(4096, 1024),
+            fc2=L.Linear(1024, 128)
+        )
+
+    def __call__(self, x):
+        h = self.conv(x)
         h = self.fc1(h)
         h = self.fc2(h)
-
         return h
