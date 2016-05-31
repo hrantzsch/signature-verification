10200363db28c1d9b00ac94b52d0b5e3bb7edac1
Hannes Rantzsch <hannes.rantzsch@student.hpi.de>
2016-05-31 11:45:01 +0200

    printing stats in hards sampling and legacy vgg

--- train_hard_triplets.py
+++ train_hard_triplets.py
@@ -11,7 +11,7 @@ from tripletembedding.aux import Logger, load_snapshot
 from aux.hard_negative_loader import HardNegativeLoader, get_samples, EpochDoneException, NotReadyException
 from aux import helpers
 
-from models.vgg_small import VGGSmall
+from models.vgg_small_legacy import VGGSmall
 
 
 def construct_data_sets(data_dir, skilled, test_ratio):
@@ -33,12 +33,12 @@ def try_get_batch(dl, model, batchsize):
 
 
 if __name__ == '__main__':
-    # time_started = time.time()
-
     args = helpers.get_args()
 
     xp = cuda.cupy if args.gpu >= 0 else np
     train, test = construct_data_sets(args.data, False, args.test)
+    print("Training set size: {}".format(len(train)))
+
     dl = HardNegativeLoader(cuda.cupy, train, 4 * args.batchsize)
 
     model = TripletNet(VGGSmall)
@@ -60,22 +60,38 @@ if __name__ == '__main__':
     dl.prepare_batch(model, args.batchsize)
 
     for _ in range(1, args.epoch + 1):
+        t_iteration = []
+        t_get_batch = []
+        t_update = []
+
         optimizer.new_epoch()
-        print('========\nepoch', optimizer.epoch)
+        print("==== epoch {} ===".format(optimizer.epoch))
         dl.reset()
         # train loop
         while True:
+            t_it_started = time.time()
             try:
                 batch = try_get_batch(dl, model, args.batchsize)
             except EpochDoneException:
                 break
+            t_get_batch.append(time.time() - t_it_started)
             # concatenate batch to format expected by tripletnet
             batch = cuda.cupy.concatenate(batch, axis=0)
 
             model.clean()
             x = chainer.Variable(batch)
+            t_up_started = time.time()
             optimizer.update(model, x)
+
+            t_update.append(time.time() - t_up_started)
+            t_iteration.append(time.time() - t_it_started)
+
             logger.log_iteration("train",
                                  float(model.loss.data), float(model.accuracy),
                                  float(model.mean_diff), float(model.max_diff))
+
+        print()
         logger.log_mean("train")
+        print("mean iteration time: {:.2f} sec".format(np.mean(t_iteration)))
+        print("mean batch sampling time: {:.2f} sec".format(np.mean(t_get_batch)))
+        print("mean update time: {:.2f} sec".format(np.mean(t_update)))
