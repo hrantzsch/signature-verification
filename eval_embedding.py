ea04dcad7fb1ec3b1b4f6667ff08a20e02c2fddf
Hannes Rantzsch <hannes.rantzsch@student.hpi.de>
2016-06-27 14:23:44 +0200

    finish eval_embeddings script

--- eval_embedding.py
+++ eval_embedding.py
@@ -6,8 +6,6 @@ Currently the script prints min, mean, and max distances intra-class and
 comparing a class's samples to the respective forgeries.
 """
 
-# TODO use sqeucledean
-
 import pickle
 import sys
 import numpy as np
@@ -15,8 +13,20 @@ from scipy.spatial.distance import cdist, pdist
 from chainer import cuda
 
 
+AVG = False
 DIST_METHOD = 'sqeuclidean'
 
+
+# ============================================================================
+
+# [*1] HACK:
+# scaling the genuine-forgery result by (num_genuines / num_forgeries) per user
+# in order to balance the number for genuines and forgeries I have
+# (see https://en.wikipedia.org/wiki/Accuracy_paradox)
+# however num_genuines depends on whether or not I average the genuines:
+# 5 if I do, 4 if I don't (not comparing a genuine with itself)
+# gen_forge_dists[user].shape[0] is however always 5; thus, reduce by one if I
+# don't take the average
 
 # ============================================================================
 
@@ -25,7 +35,10 @@ def false_positives(gen_forge_dists, threshold):
     """Number of Genuine-Forgery pairs that are closer than threshold"""
     result = 0
     for user in gen_forge_dists.keys():
-        result += np.sum(gen_forge_dists[user] <= threshold)
+        user_result = np.sum(gen_forge_dists[user] <= threshold)
+        user_result *= (gen_forge_dists[user].shape[0] - (1-AVG)) / \
+                        gen_forge_dists[user].shape[1]  # HACK [*1]
+        result += user_result
     return result
 
 
@@ -33,7 +46,10 @@ def true_negatives(gen_forge_dists, threshold):
     """Number of Genuine-Forgery pairs that are farther than threshold"""
     result = 0
     for user in gen_forge_dists.keys():
-        result += np.sum(gen_forge_dists[user] > threshold)
+        user_result = np.sum(gen_forge_dists[user] > threshold)
+        user_result *= (gen_forge_dists[user].shape[0] - (1-AVG)) / \
+                        gen_forge_dists[user].shape[1]  # HACK [*1]
+        result += user_result
     return result
 
 
@@ -58,23 +74,38 @@ def false_negatives(gen_gen_dists, threshold):
 # ============================================================================
 
 
-def genuine_genuine_dists(data):
+def genuine_genuine_dists(data, average=False):
     gen_keys = [k for k in data.keys() if 'f' not in k]
     dists = {}
     for k in gen_keys:
         gen = cuda.cupy.asnumpy(data[k])
-        dists[k] = cdist(gen, gen, DIST_METHOD)
+        if average:
+            gen_mean = []
+            for i in range(len(gen)):
+                others = list(range(len(gen)))
+                others.remove(i)
+                gen_mean.append(np.mean(gen[others], axis=0))
+            dists[k] = cdist(gen_mean, gen, DIST_METHOD)
+        else:
+            dists[k] = cdist(gen, gen, DIST_METHOD)
     return dists
 
 
-def genuine_forgeries_dists(data):
+def genuine_forgeries_dists(data, average=False):
     gen_keys = [k for k in data.keys() if 'f' not in k]
     dists = {}
     for k in gen_keys:
         gen = cuda.cupy.asnumpy(data[k])
+        if average:
+            gen_mean = []
+            for i in range(len(gen)):
+                others = list(range(len(gen)))
+                others.remove(i)
+                gen_mean.append(np.mean(gen[others], axis=0))
+            gen = gen_mean
         forge = cuda.cupy.asnumpy(data[k+'_f'])
-        np.random.shuffle(forge)
-        forge = forge[:4]  # HACK reduce number of forgeries
+        # np.random.shuffle(forge)
+        # forge = forge[:5]  # HACK reduce number of forgeries
         dists[k] = cdist(gen, forge, DIST_METHOD)
     return dists
 
@@ -85,8 +116,8 @@ def genuine_forgeries_dists(data):
 def roc(thresholds, data):
     """Compute point in a ROC curve for given thresholds.
        Returns a point (false-pos-rate, true-pos-rate)"""
-    gen_forge_dists = genuine_forgeries_dists(data)
-    gen_gen_dists = genuine_genuine_dists(data)
+    gen_gen_dists = genuine_genuine_dists(data, AVG)
+    gen_forge_dists = genuine_forgeries_dists(data, AVG)
     for i in thresholds:
         tp = true_positives(gen_gen_dists, i)
         fp = false_positives(gen_forge_dists, i)
@@ -97,6 +128,7 @@ def roc(thresholds, data):
         tpr = tp / (tp + fn)
 
         if tp + fp == 0:
+            # make sure we don't divide by zero
             f1 = 0.0
         else:
             precision = tp / (tp + fp)
@@ -145,6 +177,9 @@ def pdists_for_key(embeddings, k):
     return pdist(samples, DIST_METHOD)
 
 
+# ============================================================================
+
+
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
 
@@ -166,25 +201,26 @@ if __name__ == '__main__':
     print("Accuracy: {:.4f} (threshold = {})".format(acc[best_acc_idx],
                                                      thresholds[best_acc_idx]))
 
-    # plt.plot(fpr, tpr)
-    # plt.xlabel('False-Positive-Rate')
-    # plt.ylabel('True-Positive-Rate')
-    # plt.title('ROC curve for threshold {} to {}'.format(thresholds[0],
-    #                                                     thresholds[-1]))
-
-    # plt.figure()
-    # plt.plot(thresholds, acc)
-    # plt.xlabel('Threshold')
-    # plt.ylabel('Accuracy')
-    # plt.xticks(range(thresholds[0], thresholds[-1], 25))
-    # plt.title('Accuracy')
+    fig, axes = plt.subplots(2, sharex=True)
+    axes[0].plot(thresholds, f1)
+    axes[0].set_xlabel('Threshold')
+    axes[0].set_ylabel('F1')
+    axes[0].set_xticks(range(thresholds[0], thresholds[-1], 25))
+    axes[0].set_title('F1')
+    axes[0].set_ylim([0.0, 1.0])
+    axes[1].plot(thresholds, acc)
+    axes[1].set_xlabel('Threshold')
+    axes[1].set_ylabel('Accuracy')
+    axes[1].set_xticks(range(thresholds[0], thresholds[-1], 25))
+    axes[1].set_title('Accuracy')
+    axes[1].set_ylim([0.0, 1.0])
 
     fig, ax1 = plt.subplots()
     ax1.plot(fpr, tpr)
     ax1.set_xlabel('False-Positive-Rate')
     ax1.set_ylabel('True-Positive-Rate')
-    ax1.set_xlim([0.0, 1.0])
-    ax1.set_ylim([0.0, 1.0])
+    ax1.set_xlim([-0.05, 1.0])
+    ax1.set_ylim([0.0, 1.05])
 
     ax1.scatter([fpr[best_acc_idx]], [tpr[best_acc_idx]], c='r', edgecolor='r',
                 s=50, label='best accuracy: {:.2%} (t = {})'
@@ -193,12 +229,5 @@ if __name__ == '__main__':
                 s=50, label='best F1: {:.2%} (t = {})'
                             .format(f1[best_f1_idx], best_f1_idx))
     ax1.legend(loc='lower right', scatterpoints=1)
-
-    # trying to create a second x axis label for the threshold, but it does not
-    # align with the fpr axis...
-    # ax2 = ax1.twiny()
-    # ax2.set_xlabel('threshold')
-    # ax2.set_xticks(range(thresholds[0], thresholds[-1] + 25, 25))
-    # ax2.plot(thresholds, tpr)
 
     plt.show()
