533479d22a2c4d017992f1b29e5243f0ad4a341f
Hannes Rantzsch <hannes.rantzsch@student.hpi.de>
2016-07-01 21:30:00 +0200

    lots of evaluations

--- eval_embedding.py
+++ eval_embedding.py
@@ -36,8 +36,8 @@ def false_positives(gen_forge_dists, threshold):
     result = 0
     for user in gen_forge_dists.keys():
         user_result = np.sum(gen_forge_dists[user] <= threshold)
-        user_result *= (gen_forge_dists[user].shape[0] - (1-AVG)) / \
-                        gen_forge_dists[user].shape[1]  # HACK [*1]
+        user_result *= (gen_forge_dists[user].shape[0] - (1 - AVG)) / \
+            gen_forge_dists[user].shape[1]  # HACK [*1]
         result += user_result
     return result
 
@@ -47,8 +47,8 @@ def true_negatives(gen_forge_dists, threshold):
     result = 0
     for user in gen_forge_dists.keys():
         user_result = np.sum(gen_forge_dists[user] > threshold)
-        user_result *= (gen_forge_dists[user].shape[0] - (1-AVG)) / \
-                        gen_forge_dists[user].shape[1]  # HACK [*1]
+        user_result *= (gen_forge_dists[user].shape[0] - (1 - AVG)) / \
+            gen_forge_dists[user].shape[1]  # HACK [*1]
         result += user_result
     return result
 
@@ -74,7 +74,7 @@ def false_negatives(gen_gen_dists, threshold):
 # ============================================================================
 
 
-def genuine_genuine_dists(data, average=False):
+def genuine_genuine_dists(data, average):
     gen_keys = [k for k in data.keys() if 'f' not in k]
     dists = {}
     for k in gen_keys:
@@ -91,7 +91,7 @@ def genuine_genuine_dists(data, average=False):
     return dists
 
 
-def genuine_forgeries_dists(data, average=False):
+def genuine_forgeries_dists(data, average):
     gen_keys = [k for k in data.keys() if 'f' not in k]
     dists = {}
     for k in gen_keys:
@@ -103,7 +103,7 @@ def genuine_forgeries_dists(data, average=False):
                 others.remove(i)
                 gen_mean.append(np.mean(gen[others], axis=0))
             gen = gen_mean
-        forge = cuda.cupy.asnumpy(data[k+'_f'])
+        forge = cuda.cupy.asnumpy(data[k + '_f'])
         # np.random.shuffle(forge)
         # forge = forge[:5]  # HACK reduce number of forgeries
         dists[k] = cdist(gen, forge, DIST_METHOD)
@@ -148,7 +148,7 @@ def roc(thresholds, data):
 
 def print_stats(data):
     intra_class_dists = {k: pdists_for_key(data, k) for k in data.keys()}
-    gen_forge_dists = genuine_forgeries_dists(data)
+    gen_forge_dists = genuine_forgeries_dists(data, False)
 
     print("class\t|\tintra-class\t\t|\tforgeries")
     print("-----\t|\tmin - mean - max\t|\tmin - mean - max")
@@ -183,6 +183,56 @@ def pdists_for_key(embeddings, k):
 # ============================================================================
 
 
+def logit(x):
+    return np.log(x / (1 - x))
+
+
+def dist_to_score(dist, max_dist=500):
+    """Supposed to compute P(target_trial | s)"""
+    return max(1 / max_dist, 1 - dist / max_dist)
+
+
+def llr(dist, condition_positive_rate):
+    """Computes log-Likelihood-ratio given a sample's distance and the ratio of
+       condition positive samples in the data."""
+    # TODO provide a max_dist or find a more intelligent way
+    return logit(dist_to_score(dist)) - logit(condition_positive_rate)
+
+
+# def dist_to_conf(dist, thresh, mode='step'):
+#     # TODO: I want llr, not conf. How to get the llr from the probability?
+#     # log-likelihood-ratio: This is the logarithm of the ratio between the
+#     # likelihood that the target produced the speech input, and the likelihood
+#     # that a non-target produced the input.
+#     #
+#     # P(gen) / 1 - P(gen) ??
+#     # this is Inf for P(gen) = 1.
+#     # log of that is logit.
+#     # (http://mathworld.wolfram.com/LogitTransformation.html)
+#     #
+#     # likelihood ratio can also describe tpr / fpr
+#     # (http://mathworld.wolfram.com/LikelihoodRatio.html)
+#     # but that doesn't make sense for a single sample
+#     #
+#     if mode == 'step':
+#         return 1.0 if dist <= thresh else 0.0
+#     # if mode == 'linear':
+#     # if mode == 'sigmoid':
+#     # if mode == 'logit':
+#     else:  # default
+#         raise Exception('unknown mode')
+
+
+def save_dists(dists, out='distances.txt'):
+    """Save distances to tab delimited txt file. Expects distances as a dict
+       {'Genuine': [...], 'Forged': [...]}, where 'Genuine' contains the
+       target trial llrs."""
+    pass
+
+
+# ============================================================================
+
+
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
 
@@ -190,9 +240,28 @@ if __name__ == '__main__':
     data = pickle.load(open(data_path, 'rb'))
     # data = {'0000': data['0000'], '0000_f': data['0000_f']}
 
+# ============================================================================
+
+    dists_target = genuine_genuine_dists(data, AVG)
+    dists_target = np.concatenate([dists_target[k].ravel()
+                                   for k in dists_target.keys()])
+
+    dists_nontarget = genuine_forgeries_dists(data, AVG)
+    dists_nontarget = np.concatenate([dists_nontarget[k].ravel()
+                                      for k in dists_nontarget.keys()])
+
+    max_dist = np.max(np.concatenate((dists_target, dists_nontarget)))
+    scores_target = list(map(lambda s: dist_to_score(s, max_dist),
+                             dists_target))
+    scores_nontarget = list(map(lambda s: dist_to_score(s, max_dist),
+                            dists_nontarget))
+
+# ============================================================================
+
     print_stats(data)
 
-    thresholds = range(0, 310, 1)
+    STEP = 2.0
+    thresholds = np.arange(0.0, 150.0, STEP)
     zipped = list(roc(thresholds, data))
     (fpr, tpr, f1, acc, aer) = zip(*zipped)
 
@@ -207,33 +276,56 @@ if __name__ == '__main__':
     print("AER: {:.4f} (threshold = {})".format(aer[best_aer_idx],
                                                 thresholds[best_aer_idx]))
 
-    fig, axes = plt.subplots(2, sharex=True)
-    axes[0].plot(thresholds, f1)
-    axes[0].set_xlabel('Threshold')
-    axes[0].set_ylabel('F1')
-    axes[0].set_xticks(range(thresholds[0], thresholds[-1], 25))
-    axes[0].set_title('F1')
-    axes[0].set_ylim([0.0, 1.0])
-    axes[1].plot(thresholds, acc)
-    axes[1].set_xlabel('Threshold')
-    axes[1].set_ylabel('Accuracy')
-    axes[1].set_xticks(range(thresholds[0], thresholds[-1], 25))
-    axes[1].set_title('Accuracy')
-    axes[1].set_ylim([0.0, 1.0])
+# ============================================================================
+# F1 score and Accuracy
+# ============================================================================
 
-    fig, ax1 = plt.subplots()
-    ax1.plot(fpr, tpr)
-    ax1.set_xlabel('False-Positive-Rate')
-    ax1.set_ylabel('True-Positive-Rate')
-    ax1.set_xlim([-0.05, 1.0])
-    ax1.set_ylim([0.0, 1.05])
+    fig, acc_plot = plt.subplots(2, sharex=True)
+    acc_plot[0].plot(thresholds, f1)
+    acc_plot[0].set_xlabel('Threshold')
+    acc_plot[0].set_ylabel('F1')
+    acc_plot[0].set_xticks(np.arange(thresholds[0], thresholds[-1], STEP * 5))
+    acc_plot[0].set_title('F1')
+    acc_plot[0].set_ylim([0.0, 1.0])
+    acc_plot[1].plot(thresholds, acc)
+    acc_plot[1].set_xlabel('Threshold')
+    acc_plot[1].set_ylabel('Accuracy')
+    acc_plot[1].set_xticks(np.arange(thresholds[0], thresholds[-1], STEP * 5))
+    acc_plot[1].set_title('Accuracy')
+    acc_plot[1].set_ylim([0.0, 1.0])
 
-    ax1.scatter([fpr[best_acc_idx]], [tpr[best_acc_idx]], c='r', edgecolor='r',
-                s=50, label='best accuracy: {:.2%} (t = {})'
-                            .format(acc[best_acc_idx], best_acc_idx))
-    ax1.scatter([fpr[best_f1_idx]], [tpr[best_f1_idx]], c='g', edgecolor='g',
-                s=50, label='best F1: {:.2%} (t = {})'
-                            .format(f1[best_f1_idx], best_f1_idx))
-    ax1.legend(loc='lower right', scatterpoints=1)
+# ============================================================================
+# ROC curve
+# ============================================================================
+
+    fig, roc_plot = plt.subplots()
+    roc_plot.plot(fpr, tpr)
+    roc_plot.set_xlabel('False-Positive-Rate')
+    roc_plot.set_ylabel('True-Positive-Rate')
+    roc_plot.set_xlim([-0.05, 1.0])
+    roc_plot.set_ylim([0.0, 1.05])
+
+    roc_plot.scatter([fpr[best_acc_idx]], [tpr[best_acc_idx]],
+                     c='r', edgecolor='r', s=50,
+                     label='best accuracy: {:.2%} (t = {})'.format(
+                        acc[best_acc_idx], thresholds[best_acc_idx]))
+    roc_plot.scatter([fpr[best_f1_idx]], [tpr[best_f1_idx]],
+                     c='g', edgecolor='g', s=50,
+                     label='best F1: {:.2%} (t = {})'.format(
+                        f1[best_f1_idx], thresholds[best_f1_idx]))
+    roc_plot.legend(loc='lower right', scatterpoints=1)
+
+
+# ============================================================================
+# Histograms
+# ============================================================================
+
+    fig, hist_plots = plt.subplots(2)
+    hist_plots[0].hist(scores_target, bins='auto', range=(0.0, 1.0))
+    hist_plots[0].set_title("Histogram target trials")
+    hist_plots[1].hist(scores_nontarget, bins='auto', range=(0.0, 1.0))
+    hist_plots[1].set_title("Histogram non-target trials")
+
+# ============================================================================
 
     plt.show()
