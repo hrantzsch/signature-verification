60dc7e0f2383fea644f8100f06df6289715dbf7a
Hannes Rantzsch <hannes.rantzsch@student.hpi.de>
2016-08-03 15:41:02 +0200

    fancify pca plotting

--- tools/embeddings_plot.py
+++ tools/embeddings_plot.py
@@ -29,22 +29,38 @@ def make_pca(data):
     return result
 
 
-def get_label(name):
-    return "User {} {}".format(int(name[:4]),
-                               "Forgery" if '_f' in name else "Genuine")
+def get_label(name, num=None):
+    group = "Forgery" if '_f' in name else "Genuine"
+    name = num if num else int(name[:4])
+    return "User {} {}".format(name, group)
+
+
+def plot_class(plot, data, label, color, forgery=False, n_dims=2):
+    c = color if not forgery else 'gray'
+    ec = color if forgery else 'white'
+    lw = 0.2 if not forgery else 1.5
+    if n_dims == 3:
+        plot.scatter(data[:, 0], data[:, 1], data[:, 2],
+                     marker='o', s=100, c=c, edgecolor=ec,
+                     label=label, alpha=1.0, linewidth=lw)
+    else:
+        plot.scatter(data[:, 0], data[:, 1],
+                     marker='o', s=100, c=c, edgecolor=ec,
+                     label=label, alpha=1.0, linewidth=lw)
 
 
 def plot(data, num_classes, out, dims=2):
     keys = sorted(list(data.keys()))
 
-    colors = ['b', 'b', 'g', 'g', 'r', 'r', 'k', 'c', 'm', 'y']
+    colors = ['b', 'b', 'g', 'g', 'r', 'r',
+              'y', 'y', 'c', 'c', 'm', 'm']
     # colors = ['b', 'g', 'r', 'k', 'c', 'm', 'y']
 
     fig = plt.figure()
     if dims == 2:
         ax = fig.add_subplot(111)
-        ax.set_xlim([-20, 10])
-        ax.set_ylim([-10, 10])
+        # ax.set_xlim([-20, 10])
+        # ax.set_ylim([-10, 10])
     elif dims == 3:
         ax = fig.add_subplot(111, projection='3d')
         ax = Axes3D(fig)
@@ -54,17 +70,12 @@ def plot(data, num_classes, out, dims=2):
 
     for i in range(num_classes):
         persona = cuda.cupy.asnumpy(data[keys[i]])
-        c = colors[i % len(colors)] if '_f' not in keys[i] else 'gray'
-        ec = colors[i % len(colors)] if '_f' in keys[i] else 'white'
-        lw = 0.2 if '_f' not in keys[i] else 1.5
+        c = colors[i % len(colors)]
+        l = get_label(keys[i], i//2 + 1)
         if dims == 2:
-            ax.scatter(persona[:, 0], persona[:, 1],
-                       marker='o', s=100, c=c, edgecolor=ec,
-                       label=get_label(keys[i]), alpha=1.0, linewidth=lw)
+            plot_class(ax, persona, l, c, '_f' in keys[i], dims)
         else:
-            ax.scatter(persona[:, 0], persona[:, 1], persona[:, 2],
-                       marker='o', s=100, c=c, edgecolor=ec,
-                       label=get_label(keys[i]), alpha=1.0, linewidth=lw)
+            plot_class(ax, persona, l, c, '_f' in keys[i], dims)
     plt.legend()
     if out is not None:
         plt.savefig(out, dpi=180)
@@ -78,9 +89,10 @@ if __name__ == '__main__':
     data = make_pca(data)
 
     # limit to specified keys
-    keys = list(map(lambda x: '{:04d}'.format(x),
-                    [1, 2, 3]
-                    ))
+    # keys = list(map(lambda x: '{:04d}'.format(x),
+    #                 [1, 2, 3]
+    #                 ))
+    keys = [k for k in data.keys() if '_f' not in k][-6:]
     data_a = {key: data[key] for key in data.keys() if key in keys}
     data_b = {key + '_f': data[key + '_f']
               for key in data.keys() if key in keys}
